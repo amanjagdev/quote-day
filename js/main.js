e43c2e0d615931fff71f209ed578c89a316f8673
amanjagdev <amankumarjagdev@gmail.com>
2020-06-13 13:21:44 +0530

    thtmes added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const newQuoteBtn = document.querySelector(".new-quote-btn");
 const quoteHtml = document.querySelector(".quote");
 
+const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
+
+
 let quote;
+
+const themes = ['green','pink','yellow','orange'];
+let prevTheme = 'green';
 
 const getQuote = () => {
     fetch("https://api.quotable.io/random")
@@ -12,11 +19,20 @@ const getQuote = () => {
         });
 }
 
-const  setHtmlForQuote = (quote) => {
+const setHtmlForQuote = (quote) => {
+    const currentTheme = themes[Math.floor(Math.random() * themes.length)];
     quoteHtml.innerHTML = `
         <h4>${quote.content}</h4>
         <h5>By : "${quote.author}"</h5>
     `;
+    
+    header.classList.remove(prevTheme);
+    header.classList.add(currentTheme);
+
+    footer.classList.remove(prevTheme);
+    footer.classList.add(currentTheme);
+
+    prevTheme = currentTheme;
 }
 
 //First Time
